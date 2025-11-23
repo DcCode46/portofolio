ec30a8aa4ad2384579c7f102a5b5356079a14a1d
deayulianis <deayulianis788@gmail.com>
2025-11-23 10:19:16 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Smooth scroll untuk navbar & tombol
+const scrollLinks = document.querySelectorAll('nav ul li a, .btn');
+
+for (const link of scrollLinks) {
+  link.addEventListener('click', smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const targetId = e.currentTarget.getAttribute('href').slice(1);
+  const targetSection = document.getElementById(targetId);
+
+  if (targetSection) {
+    targetSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+}
+
+
+// Contact form submit (dummy)
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Terima kasih, pesan Anda telah terkirim!');
+  form.reset();
+});
+
